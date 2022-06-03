e877d3b18f689ada165c4f0e195e238b615c52ef
Sumaiya Haque <85180703+Sumaiya-haque@users.noreply.github.com>
2022-06-03 18:48:01 +0600

    Add Signin

--- api/models/Users.js
+++ api/models/Users.js
@@ -6,7 +6,6 @@ const userSchema = mongoose.Schema({
 
     _id: mongoose.Schema.Types.ObjectId,  // a unique id
     _id: mongoose.Schema.Types.ObjectId,  
-
     email: String,
     password: String    //email and password for users
 })
