f5bdb02ede85584edaa978d3f1383f0a7a084056
Sumaiya Haque <85180703+Sumaiya-haque@users.noreply.github.com>
2022-06-03 18:20:44 +0600

    add Signup

---
+++ api/models/Users.js
@@ -0,0 +1,11 @@
+       //Signup and Signin Schema\\
+
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,  // a unique id
+    email: String,
+    password: String    //email and password for users
+})
+
+module.exports = mongoose.model('user', userSchema);
--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const express = require('express'); //imported express package
 
 const app = express();  // using express as a fucntion (created an application)
 
-const studentRoutes = require('./api/routes/students')
+const mongoose = require('mongoose');
+
+const studentRoutes = require('./api/routes/students');
+const userRoutes = require('./api/routes/users');
+
+
+mongoose.connect('mongodb+srv://sumaiya:sumaiya@cluster0.c1w4h.mongodb.net/class-work?retryWrites=true&w=majority')
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+
+    extended: true 
+}))
+
 app.use('/students', studentRoutes);
+app.use('/users', userRoutes);
+
+
 module.exports = app;
